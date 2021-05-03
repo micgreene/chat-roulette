14ff18135863460fc8f2f564bd74600ac8c03f55
AnneThor <anne.thor@gmail.com>
2021-05-03 00:05:24 -0700

    handling login errors

--- src/auth/middleware/basic.js
+++ src/auth/middleware/basic.js
@@ -1,27 +1,5 @@
 const userModel = require('../models/User.js');
 
 module.exports = async (username, password) => {
-  // console.log("inside basic", username, password);
-  let user = await userModel.findOne({ username: username }, function (err, user) {
-    if (err) {
-      console.log('Database error');
-    }
-    if (user === null) {
-      console.log('No such user found');
-    } else {
-      //test if the password matches
-      userModel.authenticateBasic(username, password, function(err, user) {
-        console.log("valid in basic.js", user);
-        if (err) {
-          console.log(err.message || 'Database error' );
-        } else {
-          if (user) {
-            console.log(`Successful login ${user.username}`)
-          } else {
-            console.log('Incorrect login credentials');
-          }
-        }
-      })
-    }
-  });
+  return userModel.authenticateBasic(username, password);
 }
