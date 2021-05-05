3df320622c2e986f9500f6f6ce1b76cbfd1c3d47
Anne <anne.thor@gmail.com>
2021-05-04 18:48:42 -0700

    Update user.js

--- user.js
+++ user.js
@@ -14,8 +14,8 @@ const port = process.env.PORT;
 
 //create reference to host url
 
-// const host = `http://localhost:${port}`;
-const host = 'https://5f237673f2b6.ngrok.io';
+const host = `http://localhost:${port}`;
+// const host = 'https://5f237673f2b6.ngrok.io';
 
 //give socket the host URL
 const socket = io.connect(`${host}/chatter`);
